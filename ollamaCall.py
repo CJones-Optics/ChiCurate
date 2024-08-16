884ec86298d6fa1182430f4ebcd89881ab3bcddf
Cameron Jones <cameronjonesaustralia@gmail.com>
2024-08-16 16:39:06 +1000

    Added seperate user and system prompts

--- ollamaCall.py
+++ ollamaCall.py
@@ -20,51 +20,22 @@ def constructUserPrompt(prompt, data):
         outputStr += json.dumps(item)+"\n"
     return outputStr
 
-systemPrompt = """
-You are a helpful AI Research assistant.
-Your job is to read the titles of a research paper and evaluate their relvance to a researcher. The user will
-tell you what they study, and what they are interested in. They will then give you a list of article IDs and titles.
-Analyse the following list, and give a Rating of its relevance to the reader, and a justification.
-Do not put ANYTHING in the response that is not in the JSON.
+systemPrompt = ""
+with open("./systemPrompt.txt", 'r') as f:
+    s = f.read()
+    systemPrompt = s
 
-The response should be in the following format:
+# print(systemPrompt)
 
-{
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "type": "object",
-  "properties": {
-    "items": {
-      "type": "array",
-      "items": {
-        "type": "object",
-        "properties": {
-          "ArticleID": {
-            "type": "string",
-            "description": "A unique identifier for the article"
-          },
-          "Justification": {
-            "type": "string",
-            "description": "An explanation of why the article is is not relevant to the user's interests"
-          },
-          "Rating": {
-            "type": "number",
-            "format": "float",
-            "description": "A rating for the article, between 0 and 1, where 1 is the highest rating"
-          }
-        },
-        "required": ["ArticleID", "Justification", "Rating"]
-      }
-    }
-  },
-  "required": ["items"]
-}
-"""
 
-userPromptPrepend = """
-I am an optical engineer working in Free Space Optical Communication (FSOC).
-I am interested in Adaptive Optics, optical communication,photonic lanters,atmospheric turbulence and monitoring,
-wavefront sensing and control, phase control and laser communication.
-"""
+userPromptPath = "./userPrompt.txt"
+# Load the user prompt from a file into a string
+userPromptPrepend = ""
+with open(userPromptPath, 'r') as f:
+    s = f.read()
+    userPromptPrepend = s
+
+# print(userPromptPrepend)
 
 for i in range(len(titleBatches)):
     print(f"Batch {i+1} of {len(titleBatches)}")
